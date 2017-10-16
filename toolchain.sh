f0e0f6b99551c46dfc9cb5cfc66ae40d7fabf958
fazlamehrab <mehrab@vt.edu>
2017-10-16 17:10:22 -0400

    Should be working

--- toolchain.sh
+++ toolchain.sh
@@ -12,7 +12,7 @@ TARGET=$1
 NJOBS=-j"$(nproc)"
 PATH=$PATH:$PREFIX/bin
 ARCH_OPT="-mtune=native"
-export CFLAGS_FOR_TARGET="-m64 -O3 -ftree-vectorize $ARCH_OPT -target x86_64-hermitcore"
+export CFLAGS_FOR_TARGET="-m64 -O3 -ftree-vectorize $ARCH_OPT"
 export GOFLAGS_FOR_TARGET="-m64 -O3 -ftree-vectorize $ARCH_OPT"
 export FCFLAGS_FOR_TARGET="-m64 -O3 -ftree-vectorize $ARCH_OPT"
 export FFLAGS_FOR_TARGET="-m64 -O3 -ftree-vectorize $ARCH_OPT"
@@ -20,6 +20,21 @@ export CXXFLAGS_FOR_TARGET="-m64 -O3 -ftree-vectorize $ARCH_OPT"
 
 export AS_FOR_TARGET="$PREFIX/bin/x86_64-hermit-as"
 export CC_FOR_TARGET="$PREFIX/usr/local/bin/clang"
+
+export PATH
+
+# Checks prerequisites
+res=`cmake --version | grep version | sed -re "s/^cmake version ([0-9])\.([0-9])\.([0-9])$/\1\2/"`
+if [[ "$res" == "" || "$res" -lt "37" ]]; then
+	echo "Please install cmake > 3.7"
+	exit
+fi
+
+res=`which nasm`
+if [ "$res" == "" ]; then
+	echo "Please install nasm"
+	exit 
+fi
 
 echo "Build bootstrap toolchain for $TARGET with $NJOBS jobs for $PREFIX"
 sleep 1
@@ -41,7 +56,8 @@ git clone $CLONE_DEPTH https://github.com/RWTH-OS/gcc.git
 wget ftp://gcc.gnu.org/pub/gcc/infrastructure/isl-0.15.tar.bz2 -O isl-0.15.tar.bz2
 tar jxf isl-0.15.tar.bz2
 mv isl-0.15 gcc/isl
-./gcc/contrib/download_prerequisites
+cd gcc 
+cd ..
 fi
 
 if [ ! -d "hermit" ]; then
@@ -59,22 +75,6 @@ CC_FOR_TARGET="$PREFIX/usr/local/bin/clang" ./configure --target=$TARGET --prefi
 cd -
 fi
 
-if [ ! -d "tmp/binutils" ]; then
-mkdir -p tmp/binutils
-cd tmp/binutils
-../../binutils/configure --target=$TARGET --prefix=$PREFIX --with-sysroot --disable-multilib --disable-shared --disable-nls --disable-gdb --disable-libdecnumber --disable-readline --disable-sim --disable-libssp --enable-tls --enable-lto --enable-plugin && make $NJOBS && make install
-cd -
-fi
-read -p "End of Binutils"
-
-if [ ! -d "tmp/bootstrap" ]; then
-mkdir -p tmp/bootstrap
-cd tmp/bootstrap
-../../gcc/configure --target=$TARGET --prefix=$PREFIX --without-headers --disable-multilib --with-isl --enable-languages=c,c++,lto --disable-nls --disable-shared --disable-libssp --disable-libgomp --enable-threads=posix --enable-tls --enable-lto --disable-symvers && make $NJOBS all-gcc && make install-gcc
-cd -
-fi
-read -p "End of Bootstrap"
-
 if [ ! -d "tmp/llvm" ]; then
 mkdir -p tmp/llvm
 cd tmp/llvm
@@ -82,50 +82,70 @@ cmake -G "Unix Makefiles" ../../hermit-llvm && make $NJOBS
 make install DESTDIR=$PREFIX
 cd -
 fi
-read -p "End of LLVM"
+#read -p "End of LLVM"
+
+
+if [ ! -d "tmp/binutils" ]; then
+mkdir -p tmp/binutils
+cd tmp/binutils
+../../binutils/configure --target=$TARGET --prefix=$PREFIX --with-sysroot --disable-multilib --disable-shared --disable-nls --disable-gdb --disable-libdecnumber --disable-readline --disable-sim --disable-libssp --enable-tls --enable-lto --enable-plugin && make $NJOBS && make install && echo "success"
+cd -
+fi
+#read -p "End of Binutils"
+
+if [ ! -d "tmp/bootstrap" ]; then
+mkdir -p tmp/bootstrap
+cd tmp/bootstrap
+../../gcc/configure --target=$TARGET --prefix=$PREFIX --without-headers --disable-multilib --with-isl --enable-languages=c --disable-nls --disable-shared --disable-libssp --disable-libgomp --enable-threads=posix --enable-tls --enable-lto --disable-symvers && make $NJOBS all-gcc && make install-gcc && echo "success"
+cd -
+fi
+#read -p "End of Bootstrap"
 
 if [ ! -d "tmp/hermit" ]; then
 mkdir -p tmp/hermit
 cd tmp/hermit
-cmake -DHERMIT_PREFIX=$PREFIX -DBOOTSTRAP=true ../../hermit
+cmake -DHERMIT_PREFIX=$PREFIX -DCMAKE_INSTALL_PREFIX=$PREFIX -DBOOTSTRAP=true ../../hermit
 make hermit-bootstrap
-make hermit-bootstrap-install
+make hermit-bootstrap-install && echo "success"
 cd -
 fi
-read -p "End of Hermit-Bootstrap"
+#read -p "End of Hermit-Bootstrap"
 
 if [ ! -d "tmp/newlib" ]; then
+export CFLAGS_FOR_TARGET="-m64 -O3 -ftree-vectorize $ARCH_OPT -target x86_64-hermit"
 mkdir -p tmp/newlib
 cd tmp/newlib
-CC="$PREFIX/usr/local/bin/clang" ../../newlib/configure --target=$TARGET --prefix=$PREFIX --disable-shared --disable-multilib --enable-lto --enable-newlib-hw-fp --enable-newlib-io-c99-formats --enable-newlib-multithread && make $NJOBS && make install
+CC="$PREFIX/usr/local/bin/clang" ../../newlib/configure --target=$TARGET --prefix=$PREFIX --disable-shared --disable-multilib --enable-lto --enable-newlib-hw-fp --enable-newlib-io-c99-formats --enable-newlib-multithread && make $NJOBS && make install && echo "success"
 cd -
 fi
-read -p "End of Newlib"
+#read -p "End of Newlib"
 
 cd pte
-CC_FOR_TARGET="$PREFIX/usr/local/bin/clang" make && make install
+CC_FOR_TARGET="$PREFIX/usr/local/bin/clang" make && make install && echo "success"
 cd ..
-read -p "End of PTE"
+#read -p "End of PTE"
 
 if [ ! -d "tmp/gcc" ]; then
+export CFLAGS_FOR_TARGET="-m64 -O3 -ftree-vectorize $ARCH_OPT"
 mkdir -p tmp/gcc
 cd tmp/gcc
-../../gcc/configure --target=$TARGET --prefix=$PREFIX --with-newlib --with-isl --disable-multilib --without-libatomic --enable-languages=c,c++,go,fortran,lto --disable-nls --disable-shared --disable-libssp --enable-threads=posix --disable-libgomp --enable-tls --enable-lto --disable-symver && make $NJOBS && make install &> ~/gcc.out
+../../gcc/configure --target=$TARGET --prefix=$PREFIX --with-newlib --with-isl --disable-multilib --without-libatomic --enable-languages=c --disable-nls --disable-shared --disable-libssp --enable-threads=posix --disable-libgomp --enable-tls --enable-lto --disable-symver && make $NJOBS && make install &> ~/gcc.out && echo "success"
 cd -
 fi
-read -p "End of GCC"
+#read -p "End of GCC"
 
 # workaroud, compiler needs libgomp.spec to support OpenMP
 install -m 644 hermit/usr/libomp/libgomp.spec $PREFIX/$TARGET/lib
 
 if [ ! -d "tmp/final" ]; then
+export CFLAGS_FOR_TARGET="-m64 -O3 -ftree-vectorize $ARCH_OPT -target x86_64-hermit"
 mkdir -p tmp/final
 cd tmp/final
 cmake -DHERMIT_PREFIX=$PREFIX -DMTUNE=native ../../hermit
 make
-make install
+make install 
 cd -
 fi
-read -p "End of Final"
+#read -p "End of Final"
 
 cd ..
